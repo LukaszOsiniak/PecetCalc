0a2f5fdfa20c2321ac8b26d6d992e5f0f6927d66
LukaszOsiniak <lukasz.osiniak@gmail.com>
2023-07-02 19:54:55 +0200

    Initial commit

---
+++ src/main/java/com/example/PecetCalc/PecetCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.PecetCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecetCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecetCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PecetCalc/PecetCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PecetCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecetCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
